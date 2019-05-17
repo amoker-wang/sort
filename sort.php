c512e8472a1614cc8d2bc70152702c9651e0b84e
amoker <43432049+amoker-wang@users.noreply.github.com>
2019-05-17 18:34:36 +0800

    Update sort.php

--- sort.php
+++ sort.php
@@ -1,26 +1,29 @@
 <?php
+    //交换数组元素
     function swap(&$array,$i,$j){
         $temp=$array[$i];
         $array[$i]=$array[$j];
         $array[$j]=$temp;
     }
-    //冒泡
-    function  BubbleSort($array){
-        $flag=true;
+    //冒泡排序
+    function bubbleSort($array){
+        if(count($array)<=1)  return $array;
+        $flag= true;
         for($i=0;$i<count($array) && $flag;$i++){
-            $flag=false;
             for($j=count($array)-1;$j>$i;$j--){
-                if($array[$j] < $array[$j-1]){
+                $flag =false;
+                if($array[$j]<$array[$j-1]){
+                    $flag =true;
                     swap($array,$j,$j-1);
-                    $flag=true;
                 }
             }
         }
         return $array;
     }
+    //选择排序
+    function selectSort($array){
 
-    //简单选择排序
-    function SelectSort($array){
+        if(count($array)<=1)  return $array;
         for($i=0;$i<count($array);$i++){
             $min=$i;
             for($j=$i+1;$j<count($array);$j++){
@@ -28,77 +31,94 @@
                     $min=$j;
                 }
             }
-            if($min !=$i){
-                swap($array,$min,$i);
+            if($i !=$min){
+                swap($array,$i,$min);
             }
+
         }
         return $array;
     }
-   
     //插入排序
-    function InsertSort($arr) {
-        $len=count($arr);
-        for ($i=1; $i < $len; $i++) {
-            $tmp = $arr[$i];
-            for ($j = $i - 1; $j >= 0; $j--) {
-                     if ($tmp < $arr[$j]) {
-                    $arr[$j + 1] = $arr[$j];
-                    $arr[$j] = $tmp;
-                } else {
-                    //如果碰到不需要移动的元素
-                    //由于是已经排序好是数组，则前面的就不需要再次比较了。
+    function insertSort($array){
+
+        if(count($array)<=1)  return $array;
+
+        for($i=1;$i<count($array);$i++){
+            $temp =$array[$i];
+            for($j=$i-1;$j>=0;$j--){
+                if($temp < $array[$j]){
+                    $array[$j+1]=$array[$j];
+                    $array[$j] =$temp;
+                }else{
                     break;
                 }
             }
         }
-        return $arr;
+        return $array;
     }
-
-    //快速排序  取巧
-    function quick_sort($array){
-        // 判断是否需要运行，因下面已拿出一个中间值，这里<=1
-        if (count($array) <= 1) {
-            return $array;
-        }
-
-        $middle = $array[0]; // 中间值
-        $left = array(); // 接收小于中间值
-        $right = array();// 接收大于中间值
-
-        // 循环比较
-        for ($i=1; $i < count($array); $i++) {
-
-            if ($middle < $array[$i]) {
-                // 大于中间值
-                $right[] = $array[$i];
-            } else {
-                // 小于中间值
-                $left[] = $array[$i];
+    //快速排序 1 取巧版
+    function quickSort1($array){
+        if(count($array)<=1)  return $array;
+        $leftArr=[];
+        $rightArr=[];
+        $mid =$array[0];
+        for($i=0;$i<count($array);$i++){
+            if($array[$i] < $mid){
+                $leftArr[]=$array[$i];
+            }
+            if($array[$i] > $mid){
+                $rightArr[] =$array[$i];
             }
         }
+        $leftArr=quickSort1($leftArr);
+        $rightArr=quickSort1($rightArr);
 
-        // 递归排序划分好的2边
-        $left = quick_sort($left);
-        $right = quick_sort($right);
-
-        // 合并排序后的数据，别忘了合并中间值
-        return array_merge($left, array($middle), $right);
+        return array_merge($leftArr, array($mid), $rightArr);
     }
-  // 快速排序
-function quick_sort_swap(&$array, $start, $end) {
-    if($end <= $start) return;
-    $key = $array[$start];
-    $left = $start;
-    $right = $end;
-    while($left < $right) {
-        while($left < $right && $array[$right] > $key)
-            $right--;
-        $array[$left] = $array[$right];
-        while($left < $right && $array[$left] < $key)
-            $left++;
-        $array[$right] = $array[$left];
+    //插入排序，比较交换
+    function quick_sort_swap(&$array, $start, $end) {
+        if($end <= $start) return;
+        $key = $array[$start];
+        $left = $start;
+        $right = $end;
+        while($left < $right) {
+            while($left < $right && $array[$right] > $key)
+                $right--;
+            $array[$left] = $array[$right];
+            while($left < $right && $array[$left] < $key)
+                $left++;
+            $array[$right] = $array[$left];
+        }
+        $array[$right] = $key;
+        quick_sort_swap($array, $start, $right - 1);
+        quick_sort_swap($array, $right+1, $end);
     }
-    $array[$right] = $key;
-    quick_sort_swap($array, $start, $right - 1);
-    quick_sort_swap($array, $right+1, $end);
-}
+    //归并排序
+    function mergeSort(&$array,$left,$right){
+        if($left < $right){
+            $mid = floor(($left+$right)/2);
+            mergeSort($array,$left,$mid);
+            mergeSort($array,$mid+1,$right);
+            $i =$left;
+            $j =$mid+1;
+            $t=0;
+            $tmp=[];
+            while($i<=$mid && $j<=$right){
+                if($array[$i] <= $array[$j]){
+                    $tmp[$t++]=$array[$i++];
+                }else{
+                    $tmp[$t++]=$array[$j++];
+                }
+            }
+            while($i<=$mid){
+                $tmp[$t++]=$array[$i++];
+            }
+            while($j<=$right){
+                $tmp[$t++]=$array[$j++];
+            }
+            $t=0;
+            while($left<=$right){
+                $array[$left++]=$tmp[$t++];
+            }
+        }
+    }
